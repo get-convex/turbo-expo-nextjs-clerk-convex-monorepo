5959ec60cac6036b0bc646353d6d51ed5ec63a57
Hassan El Mghari <hassan4709@gmail.com>
2024-01-23 00:12:45 -0800

    minor fixes

--- apps/web/src/components/Header.tsx
+++ apps/web/src/components/Header.tsx
@@ -24,27 +24,27 @@ export default function Header() {
   const pathname = usePathname();
 
   return (
-    <Disclosure as="nav" className=" ">
+    <Disclosure as='nav' className=' '>
       {({ open }) => (
         <>
-          <div className="flex items-center bg-white h-16 sm:h-20">
-            <div className="container px-2 sm:px-0">
-              <div className="relative flex h-16 items-center justify-between">
-                <div className="flex sm:hidden flex-shrink-0 items-center">
+          <div className='flex items-center bg-white h-16 sm:h-20'>
+            <div className='container px-2 sm:px-0'>
+              <div className='relative flex h-16 items-center justify-between'>
+                <div className='flex sm:hidden flex-shrink-0 items-center'>
                   <Logo isMobile={true} />
                 </div>
-                <div className="sm:flex hidden flex-shrink-0 items-center">
+                <div className='sm:flex hidden flex-shrink-0 items-center'>
                   <Logo />
                 </div>
                 {pathname === '/' && (
-                  <div className="flex flex-1 items-center justify-center ">
-                    <div className="hidden sm:ml-6 sm:block">
-                      <ul className="flex space-x-28">
+                  <div className='flex flex-1 items-center justify-center '>
+                    <div className='hidden sm:ml-6 sm:block'>
+                      <ul className='flex space-x-28'>
                         {navigation.map((item) => (
                           <li key={item.name}>
                             <Link
                               href={item.href}
-                              className="text-[#2D2D2D] text-center text-xl not-italic font-normal leading-[normal]"
+                              className='text-[#2D2D2D] text-center text-xl not-italic font-normal leading-[normal]'
                               aria-current={item.current ? 'page' : undefined}
                             >
                               {item.name}
@@ -56,11 +56,11 @@ export default function Header() {
                   </div>
                 )}
                 {user ? (
-                  <div className="hidden sm:flex absolute inset-y-0 right-0 gap-6 items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                    <Link href="/notes">
+                  <div className='hidden sm:flex absolute inset-y-0 right-0 gap-6 items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0'>
+                    <Link href='/notes'>
                       <button
-                        type="button"
-                        className=" text-white text-center text-xl not-italic font-normal leading-[normal] font-montserrat px-[22px] py-[11px] button"
+                        type='button'
+                        className=' text-white text-center text-xl not-italic font-normal leading-[normal] font-montserrat px-[22px] py-[11px] button'
                       >
                         See your Notes
                       </button>
@@ -72,32 +72,30 @@ export default function Header() {
                     />
                   </div>
                 ) : (
-                  <div className="hidden sm:flex absolute inset-y-0 right-0 gap-6 items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                    <button
-                      type="button"
-                      className="border rounded-lg border-solid border-[#2D2D2D] text-[#2D2D2D] text-center text-xl not-italic font-normal leading-[normal] font-montserrat px-[22px] py-2.5"
+                  <div className='hidden sm:flex absolute inset-y-0 right-0 gap-6 items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0'>
+                    <Link
+                      href='/notes'
+                      className='border rounded-lg border-solid border-[#2D2D2D] text-[#2D2D2D] text-center text-xl not-italic font-normal leading-[normal] font-montserrat px-[22px] py-2.5'
                     >
                       Sign in
-                    </button>
-                    <Link href="/notes">
-                      <button
-                        type="button"
-                        className=" text-white text-center text-xl not-italic font-normal leading-[normal] font-montserrat px-[22px] py-[11px] button"
-                      >
-                        Get Started
-                      </button>
+                    </Link>
+                    <Link
+                      href='/notes'
+                      className=' text-white text-center text-xl not-italic font-normal leading-[normal] font-montserrat px-[22px] py-[11px] button'
+                    >
+                      Get Started
                     </Link>
                   </div>
                 )}
-                <div className="block sm:hidden">
+                <div className='block sm:hidden'>
                   {/* Mobile menu button*/}
-                  <Disclosure.Button className="relative inline-flex  items-center justify-center rounded-md p-2 text-gray-400 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                    <span className="absolute -inset-0.5" />
-                    <span className="sr-only">Open main menu</span>
+                  <Disclosure.Button className='relative inline-flex  items-center justify-center rounded-md p-2 text-gray-400 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white'>
+                    <span className='absolute -inset-0.5' />
+                    <span className='sr-only'>Open main menu</span>
                     {open ? (
-                      <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                      <XMarkIcon className='block h-6 w-6' aria-hidden='true' />
                     ) : (
-                      <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                      <Bars3Icon className='block h-6 w-6' aria-hidden='true' />
                     )}
                   </Disclosure.Button>
                 </div>
@@ -105,33 +103,31 @@ export default function Header() {
             </div>
           </div>
 
-          <Disclosure.Panel className="sm:hidden">
-            <div className="space-y-1 px-2 pb-3 pt-2 flex flex-col gap-3 items-start">
+          <Disclosure.Panel className='sm:hidden'>
+            <div className='space-y-1 px-2 pb-3 pt-2 flex flex-col gap-3 items-start'>
               {navigation.map((item) => (
                 <Disclosure.Button
                   key={item.name}
                   as={Link}
                   href={item.href}
-                  className="text-[#2D2D2D] text-center text-xl not-italic font-normal leading-[normal]"
+                  className='text-[#2D2D2D] text-center text-xl not-italic font-normal leading-[normal]'
                   aria-current={item.current ? 'page' : undefined}
                 >
                   {item.name}
                 </Disclosure.Button>
               ))}
-              <div className="flex gap-6 items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                <button
-                  type="button"
-                  className="border rounded-lg border-solid border-[#2D2D2D] text-[#2D2D2D] text-center text-xl not-italic font-normal leading-[normal] font-montserrat px-5 py-[5px]"
+              <div className='flex gap-6 items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0'>
+                <Link
+                  href='/notes'
+                  className='border rounded-lg border-solid border-[#2D2D2D] text-[#2D2D2D] text-center text-xl not-italic font-normal leading-[normal] font-montserrat px-5 py-[5px]'
                 >
                   Sign in
-                </button>
-                <Link href="/notes">
-                  <button
-                    type="button"
-                    className=" text-white text-center text-xl not-italic font-normal leading-[normal] font-montserrat px-5 py-1.5 button"
-                  >
-                    Get Started
-                  </button>
+                </Link>
+                <Link
+                  href='/notes'
+                  className=' text-white text-center text-xl not-italic font-normal leading-[normal] font-montserrat px-5 py-1.5 button'
+                >
+                  Get Started
                 </Link>
               </div>
             </div>
