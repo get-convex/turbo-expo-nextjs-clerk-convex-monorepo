9bd5168a774f5aea0317fed2f240375448d41cac
Ian Macartney <ianhmacartney@gmail.com>
2024-02-04 18:47:21 -0800

    env variable nit

--- packages/db/convex/utils.ts
+++ packages/db/convex/utils.ts
@@ -1,14 +1,16 @@
 export function missingEnvVariableUrl(envVarName: string, whereToGet: string) {
-  const deploymentName = process.env.CONVEX_CLOUD_URL?.slice(8).replace(
-    ".convex.cloud",
-    ""
-  );
+  const deployment = deploymentName();
+  if (!deployment) return `Missing ${envVarName} in environment variables.`;
   return (
     `\n  Missing ${envVarName} in environment variables.\n\n` +
-    `  Get it from ${whereToGet} .\n` +
-    "  Paste it on the Convex dashboard:\n" +
-    "  https://dashboard.convex.dev/d/" +
-    deploymentName +
-    `/settings?var=${envVarName}`
+    `  Get it from ${whereToGet} .\n  Paste it on the Convex dashboard:\n` +
+    `  https://dashboard.convex.dev/d/${deployment}/settings?var=${envVarName}`
   );
+}
+
+export function deploymentName() {
+  const url = process.env.CONVEX_CLOUD_URL;
+  if (!url) return undefined;
+  const regex = new RegExp('https://(.+).convex.cloud');
+  return regex.exec(url)?.[1];
 }
