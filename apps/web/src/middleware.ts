7999d4d1c339c80213f9509fe07d34fe3e23535e
Michal Srb <srb@convex.dev>
2024-05-13 16:04:15 +0200

    Upgrade clerk middleware

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -1,7 +1,14 @@
-import { authMiddleware } from "@clerk/nextjs";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
-export default authMiddleware({
-  publicRoutes: ["/"],
+const isProtectedRoute = createRouteMatcher(["/notes(.*)"]);
+
+export default clerkMiddleware((auth, request) => {
+  if (isProtectedRoute(request)) {
+    auth().protect();
+  }
+
+  return NextResponse.next();
 });
 
 export const config = {
